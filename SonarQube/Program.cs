89f9882641f92c23fdfa05fcf91f325628d7dded
Chandru Mariappan <Chandru.Mariappan@tenadroit.com>
2022-08-24 21:12:50 +0530

    first commit

---
+++ SonarQube/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SonarQube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
