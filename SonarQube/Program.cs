cd5a2b202185ec5b1dbaf42ca1f0665cc57b0965
chand <chand@DESKTOP-OG4TMVM>
2022-10-27 06:26:55 -0700

    Chandra

--- SonarQube/Program.cs
+++ SonarQube/Program.cs
@@ -9,7 +9,8 @@ namespace SonarQube
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World2!");
             Console.WriteLine("Hello World2132WEWE!");
-            
+            Console.WriteLine("Hello Chandrasekar");
+
         }
     }
 }
