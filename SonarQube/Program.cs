6fba1ce1a6bfc2c07955cf4562cad95d30712296
chandrasekarMariappan <chandrumca11@gmail.com>
2022-10-27 00:35:08 -0700

    ZDSAD
    ADSADSA

--- SonarQube/Program.cs
+++ SonarQube/Program.cs
@@ -7,7 +7,7 @@ namespace SonarQube
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-                        Console.WriteLine("Hello World2!");
+            Console.WriteLine("Hello World2!");
             Console.WriteLine("Hello World2132WEWE!");
         }
     }
