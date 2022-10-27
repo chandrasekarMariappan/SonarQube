71e3a3d44d63c89762bd6182012bbe5513a58f81
chandrasekarMariappan <chandrumca11@gmail.com>
2022-10-27 00:02:54 -0700

    new changes
    Thanks

--- SonarQube/Program.cs
+++ SonarQube/Program.cs
@@ -7,6 +7,7 @@ namespace SonarQube
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+                        Console.WriteLine("Hello World2!");
         }
     }
 }
